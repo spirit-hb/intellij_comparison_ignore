d33f59b42f1851c83b931ba8d6511ef27ca19c0b
GitHub Action <action@github.com>
2023-02-01 12:13:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/spirithb/intellijcomparisonignore/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/spirithb/intellijcomparisonignore/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.spirithb.intellijcomparisonignore.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.spirithb.intellijcomparisonignore.MyBundle
 
 class MyApplicationService {
 
